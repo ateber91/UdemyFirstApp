58b681cb7c8824cd4a5c6e9b81acc143a0e80181
Ali Teber <ateber91@gmail.com>
2019-07-25 07:19:18 -0400

    LD changes

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,37 +1,37 @@
 import { Component, OnInit } from '@angular/core';
-import LDClient from 'ldclient-js';
+import LDClient from 'launchdarkly-js-client-sdk';
 
 @Component({
     selector: 'app-auth',
     templateUrl: './auth.component.html',
-    styleUrls: [ './auth.component.css' ]
+    styleUrls: ['./auth.component.css']
 })
 export class AuthComponent implements OnInit {
     ldclient;
     turnAuthOn = false;
-    constructor () {}
-    ngOnInit () {
+    constructor() { }
+    ngOnInit() {
         console.log('terst');
         this.componentDidMount();
     }
 
-    componentDidMount () {
+    componentDidMount() {
         const user = {
-            firstName: 'Bob',
-            lastName: 'Loblaw',
-            key: 'UNIQUE IDENTIFIER',
+            firstName: 'Georgi',
+            lastName: 'Petrov',
+            key: 'UNIQUE IDENTIFIER1',
             country: 'Bulgaria',
             custom: {
                 groups: 'beta_testers'
             }
         };
         this.ldclient = LDClient.initialize('5d19ed30eae18b07640dc624', user);
-        this.ldclient.on('ready', this.onLaunchDarklyReady.bind(this));
-        this.ldclient.on('change', this.onLaunchDarklyReady.bind(this));
+        this.ldclient.on('ready', this.onLaunchDarklyReady);
+        this.ldclient.on('change', this.onLaunchDarklyReady);
     }
-    onLaunchDarklyReady () {
+    onLaunchDarklyReady = () => {
         this.turnAuthOn = this.ldclient.variation('enable-auth');
         console.log(this.ldclient.getUser());
         console.log(this.ldclient.variationDetail('enable-auth'));
-    }
+    };
 }
